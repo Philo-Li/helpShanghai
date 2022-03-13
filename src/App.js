c47549a08ee1e4b04dd79572d741380705b0d624
Philo <maerd.lll.p@gmail.com>
2022-03-13 14:04:45 +0800

    remove unused component

--- src/App.js
+++ src/App.js
@@ -6,10 +6,8 @@ import {
   BrowserRouter as Router,
   Switch, Route, Redirect,
 } from 'react-router-dom';
-import { useMoralis } from 'react-moralis';
 import { Nav, NavDropdown } from 'react-bootstrap';
 import Navbar from 'react-bootstrap/Navbar';
-
 import Home from './components/home/Home';
 import Discover from './components/discover/Discover';
 import Footer from './components/Footer';
@@ -29,8 +27,6 @@ import About from './components/about/About';
 import AboutZh from './components/about/AboutZh';
 import ContactUs from './components/ContactUs';
 import Create from './components/create/Create';
-import NFTBalance from './components/nft-market/NFTBalance';
-import NFTList from './components/nft-market/NFTList';
 import logo from './img/logo/logo2.svg';
 import './index.css';
 
@@ -44,7 +40,6 @@ const App = () => {
   const { authorizedUser } = useAuthorizedUser();
   const searchValue = useField('searchValue');
   const [newSearchValue, setNewSearchValue] = useState('');
-  const { authenticate } = useMoralis();
 
   const Menu = () => {
     const token = localStorage.getItem('philoart-token');
@@ -66,9 +61,9 @@ const App = () => {
               src={logo}
               width="30"
               height="30"
-              alt="PhiloArt brand logo"
+              alt="Waldon brand logo"
             />
-            PhiloArt
+            Waldon
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
@@ -78,12 +73,6 @@ const App = () => {
               </div>
             </Nav>
             <Nav className="justify-content-end container-row-0">
-              <NavDropdown title="NFTs" id="basic-nav-dropdown" style={navStyle}>
-                <NavDropdown.Item href="/nftmarket">NFT Market</NavDropdown.Item>
-                <NavDropdown.Item href="/nftlist">NFT List</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <button className="navbar-button-logout" type="submit" onClick={() => authenticate()}>Wallet</button>
-              </NavDropdown>
               <a className="navbar-link" href="/discover">Discover</a>
               <a className="navbar-link" href="/about">About</a>
               {!token && <a className="navbar-link" href="/signin">Login</a>}
@@ -112,18 +101,6 @@ const App = () => {
           <Switch>
             <Route path="/discover" exact>
               <Discover />
-            </Route>
-            <Route path="/nftmarket" exact>
-              <NFTBalance />
-            </Route>
-            <Route path="/nftbalance" exact>
-              <NFTBalance />
-            </Route>
-            <Route path="/transactions" exact>
-              <NFTBalance />
-            </Route>
-            <Route path="/nftlist" exact>
-              <NFTList />
             </Route>
             <Route path="/license" exact>
               <License />
