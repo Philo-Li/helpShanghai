f67075f7c6bb7a53e2965cbc23d8aae85e06fd8a
Leilei Li <maerd.lll.p@gmail.com>
2022-04-10 23:40:16 +0800

    update mental health page

---
+++ src/components/discover/MentalHealth.js
@@ -0,0 +1,21 @@
+/* eslint-disable max-len */
+import React from 'react';
+
+const MentalHealth = () => (
+  <div className="p-3 discover">
+    <div className="p-3">
+      <div className="container-profile profile-item subheader">
+        <p>心理健康自救互助指南</p>
+      </div>
+    </div>
+    <div className="container-col-article-details">
+      <div className="container-row-license">
+        <div className="container-row-license-item">
+          1.待补充中
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default MentalHealth;
